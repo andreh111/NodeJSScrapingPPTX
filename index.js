9ef7b16ad39e50eb1fa0f50ee6045ebf3ed86d05
andreh111 <andrehabboud1@gmail.com>
2021-09-25 23:49:29 +0200

    intial commit for gdg nodejs crash course

---
+++ index.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const pptxgen = require("pptxgenjs");
+let pptx = new pptxgen();
+
+
+const url = "https://www.learnpick.in/prime/documents/ppts/details/1360/node-js-introduction";
+
+// Async function which scrapes the data
+async function scrapeData() {
+  try {
+    // Fetch HTML of the page we want to scrape
+    const response = await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(response.data);
+    // Select all the list items in plainlist class
+    const listItems = $(".item img");
+
+    const universal = $(".loaderuniversal");
+ 
+    listItems.each((idx, el) => {
+
+
+
+        let x = el.attribs.src;
+
+        console.log(x);
+
+        let slide = pptx.addSlide();
+
+        slide.addImage({ path: x,w:'100%',h:'100%' });
+      
+    });
+    pptx.writeFile({ fileName: "nodejs-presentation.pptx" });
+
+
+
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+// Invoke the above function
+scrapeData();
+
+console.log('hello')
